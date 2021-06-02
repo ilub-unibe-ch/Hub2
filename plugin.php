c2aa9379935f6f1c222bb01a281edb36a75b1478
root <root@ilias-webng2.unibe.ch>
2021-06-02 14:27:09 +0200

    Update for ilias7

--- plugin.php
+++ plugin.php
@@ -5,6 +5,6 @@ require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 $id = 'hub2';
 $version = '2.2.5';
 $ilias_min_version = '5.3.0';
-$ilias_max_version = '6.999.999';
+$ilias_max_version = '7.999.999';
 $responsible = 'studer + raimann ag - Team Custom 1';
 $responsible_mail = 'support-custom1@studer-raimann.ch';
