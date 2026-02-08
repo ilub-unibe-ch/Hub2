5b52f2b111bb49fabfce075dedb303095aac65d4
Bahwar <bahwar.adi@unibe.ch>
2026-02-08 14:46:27 +0100

    read retention and batching from plugin settings

--- src/Jobs/SyncCleanupJob.php
+++ src/Jobs/SyncCleanupJob.php
@@ -4,6 +4,7 @@ namespace srag\Plugins\Hub2\Jobs;
 
 use ilCronJobResult;
 use ILIAS\Cron\Schedule\CronJobScheduleType;
+use srag\Plugins\Hub2\Jobs\Sync\Cleanup\CleanupSettings;
 use srag\Plugins\Hub2\Jobs\Sync\Cleanup\SyncCleanupService;
 use srag\Plugins\Hub2\Jobs\Sync\Persistence\DbAdHocDataRepository;
 use srag\Plugins\Hub2\Log\ILog;
@@ -12,9 +13,6 @@ use srag\Plugins\Hub2\Log\Repository as LogRepo;
 class SyncCleanupJob extends \ilCronJob
 {
     public const CRON_JOB_ID = "Sync-Cleanup-Job";
-    private const RETENTION_DAYS = 7;
-    private const BATCH_SIZE = 10;
-    private const MAX_BATCHES = 200;
 
 
     public function getId(): string
@@ -68,8 +66,14 @@ class SyncCleanupJob extends \ilCronJob
         $runId = bin2hex(random_bytes(4));
         $startedAt = microtime(true);
 
+        $cleanupSettings = new CleanupSettings();
+
+        $retentionDays = $cleanupSettings->getRetentionDays();
+        $batchSize     = $cleanupSettings->getBatchSize();
+        $maxBatches    = $cleanupSettings->getMaxBatches();
+
         $cutoff = (new \DateTimeImmutable('now', new \DateTimeZone('UTC')))
-            ->modify("-" . self::RETENTION_DAYS . " days")
+            ->modify("-" . $retentionDays . " days")
             ->format('Y-m-d H:i:s');
 
         try {
@@ -78,8 +82,8 @@ class SyncCleanupJob extends \ilCronJob
 
             $summary = $service->cleanupProcessedBefore(
                 $cutoff,
-                self::BATCH_SIZE,
-                self::MAX_BATCHES,
+                $batchSize,
+                $maxBatches,
                 null
             );
 
@@ -105,9 +109,9 @@ class SyncCleanupJob extends \ilCronJob
                 ->withTitle('Cron: SyncCleanup')
                 ->addAdditionalData('runId', $runId)
                 ->addAdditionalData('cutoffUtc', $cutoff)
-                ->addAdditionalData('retentionDays', self::RETENTION_DAYS)
-                ->addAdditionalData('batchSize', self::BATCH_SIZE)
-                ->addAdditionalData('maxBatches', self::MAX_BATCHES)
+                ->addAdditionalData('retentionDays', $retentionDays)
+                ->addAdditionalData('batchSize', $batchSize)
+                ->addAdditionalData('maxBatches', $maxBatches)
                 ->addAdditionalData('deleted', $summary->deleted)
                 ->addAdditionalData('batches', $summary->batches)
                 ->addAdditionalData('stoppedByLimit', $summary->stoppedByLimit)
@@ -130,9 +134,9 @@ class SyncCleanupJob extends \ilCronJob
                 ->addAdditionalData('runId', $runId)
                 ->addAdditionalData('cutoffUtc', $cutoff)
                 ->addAdditionalData('durationMs', $durationMs)
-                ->addAdditionalData('retentionDays', self::RETENTION_DAYS)
-                ->addAdditionalData('batchSize', self::BATCH_SIZE)
-                ->addAdditionalData('maxBatches', self::MAX_BATCHES)
+                ->addAdditionalData('retentionDays', $retentionDays)
+                ->addAdditionalData('batchSize', $batchSize)
+                ->addAdditionalData('maxBatches', $maxBatches)
                 ->addAdditionalData('exception', get_class($e))
                 ->addAdditionalData('file', $e->getFile())
                 ->addAdditionalData('line', $e->getLine())
